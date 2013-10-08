dbdf4547e3c41ee27fc0a38ee53482abfe274f58
fima <edejin@azigoinc.com>
2013-10-08 12:31:55 +0300

    fix tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
     copy: {
       main: {
         files: [
-          {cwd: './test/fixtures/', src: ['testing2'], dest: './tmp/', filter: 'isFile'}, // includes files in path
+          {cwd: './test/fixtures/', src: ['*2'], dest: './tmp/', filter: 'isFile'}, // includes files in path
         ]
       }
     }
